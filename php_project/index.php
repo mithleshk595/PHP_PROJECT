2dab46b4d1c3d6a49dfbc2fc8ec0846d2fd0f139
Mithleshk595 <mithleshk0982@gmail.com>
2026-09-15 17:33:30 +0530

    Added index.php

---
+++ php_project/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Website</title>
+</head>
+<body>
+
+    <h1>Welcome to my website</h1>
+
+    <p>This is HTML inside PHP file.</p>
+
+    <?php
+        echo "<h2>Hello Mithlesh!</h2>";
+    ?>
+
+</body>
+</html>
---
+++ php_project/welcome.php
@@ -0,0 +1,7 @@
+<?php
+
+$name = $_POST['username'];
+
+echo "<h1>Hello, $name!</h1>";
+
+?>
