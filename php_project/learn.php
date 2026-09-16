434ccde04d1bc1bb55e28139ecf0e3208e697396
Mithleshk595 <mithleshk0982@gmail.com>
2026-09-16 18:39:59 +0530

    Added learn.php

--- php_project/learn.php
+++ php_project/learn.php
@@ -6,8 +6,8 @@
 $txt1 = "Learn PHP";
 $txt2 = "W3Schools.com";
 
-echo "<h2>$txt1</h2>";
-echo "<p>Study PHP at $txt2</p>";
+echo '<h2>' . $txt1 . '</h2>';
+echo '<p>Study PHP at ' . $txt2 . '</p>';
 ?>
 
 </body>
